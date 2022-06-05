0ed861b10e50bf6365c0e427d369a06e40941c2b
alfons <alfonsspj@gmail.com>
2022-06-05 00:30:45 -0300

    Consulta a la api para obtener los Clientes

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import { useState, useEffect} from 'react'
 
 const Inicio = () => {
+
+  const [clientes, setClientes] = useState([])
+
+  useEffect(() => {
+    const obtenerClientesAPI = async () => {
+      try {
+        const url = 'http://localhost:4000/clientes'
+        const respuesta = await fetch(url) //metodo get
+        const resultado = await respuesta.json()
+
+        setClientes(resultado)
+
+      } catch (error) {
+        console.log(error);
+      }
+
+    }
+
+    obtenerClientesAPI()
+  }, [])
   return (
     <div>
         <h1>desde inicio</h1>
