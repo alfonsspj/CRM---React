49b7290a87e9ba7043ae84974c228ada28c737a0
alfons <alfonsspj@gmail.com>
2022-06-07 01:50:20 -0300

    Editar - Actualizando registros en la API

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -27,22 +27,36 @@ const Formulario = ({cliente, cargando}) => {
     })
 
     const handleSubmit = async(valores) => {
-        // console.log(valores);
+        
         try {
-            const url = 'http://localhost:4000/clientes'
+            let respuesta
+            if(cliente.id) {
+                // Editando un Registro
+                const url = `http://localhost:4000/clientes/${cliente.id}`
 
-            const respuesta = await  fetch(url, {
-                method: 'POST',
-                body: JSON.stringify(valores),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
+                respuesta = await  fetch(url, {
+                    method: 'PUT',
+                    body: JSON.stringify(valores),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
 
-            console.log(respuesta)
-            const resultado = await respuesta.json()
-            console.log(resultado)
+            }else{
+                // Nuevo Registro
+                const url = 'http://localhost:4000/clientes'
 
+                respuesta = await  fetch(url, {
+                    method: 'POST',
+                    body: JSON.stringify(valores),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+            }
+
+            await respuesta.json()
+            // console.log(resultado)
             navigate('/clientes')
               
         } catch (error) {
